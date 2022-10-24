c4396458ebad2eb24551fa51b9ba1bbbfd58b644
joshrutta <joshrutta7@gmail.com>
2022-10-23 22:35:06 -0400

    part 4 ex  4.1-4.2

--- notes-backend/mongo.js
+++ notes-backend/mongo.js
@@ -1,24 +1,24 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>')
-  process.exit(1)
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
 }
 
 const password = process.argv[2]
 
 const url = `mongodb+srv://jrutta:${password}@cluster0.cedb4py.mongodb.net/noteApp?retryWrites=true&w=majority`
 const noteSchema = new mongoose.Schema({
-  content: {
-    type: String,
-    minLength: 5,
-    required: true
-  },
-  date: {
-    type: Date,
-    required: true
-  },
-  important: Boolean,
+    content: {
+        type: String,
+        minLength: 5,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    important: Boolean,
 })
 
 const Note = mongoose.model('Note', noteSchema)
@@ -46,13 +46,13 @@ const Note = mongoose.model('Note', noteSchema)
 
 // Fetching objects from db
 mongoose
-  .connect(url)
-  .then(() => {
-    console.log('connected')
-    Note.find().then(result => {
-      result.forEach(note => {
-        console.log(note)
-      })
-      mongoose.connection.close()
+    .connect(url)
+    .then(() => {
+        console.log('connected')
+        Note.find().then(result => {
+            result.forEach(note => {
+                console.log(note)
+            })
+            mongoose.connection.close()
+        })
     })
-  })
